0c77288a382567ec68e5f0561c191006bf397734
Anuj Shah <anujshah031198@gmail.com>
2023-06-30 12:23:26 -0500

    Updated api and web pages routes

--- internal/routes/api.go
+++ internal/routes/api.go
@@ -1,16 +1,32 @@
 package routes
 
 import (
+	"net/http"
+
 	"github.com/anujshah3/AddressTrail/internal/handlers"
+	"github.com/anujshah3/AddressTrail/internal/middleware"
 	"github.com/gin-gonic/gin"
 )
 
-func SetupAPIRoutes(router *gin.Engine) {
-	router.POST("/api/users", handlers.AddNewUserHandler)
-	router.DELETE("/api/users", handlers.DeleteUserHandler)
+	func SetupAPIRoutes(router *gin.Engine) {
+		router.POST("/api/users", handlers.AddNewUserHandler)
+		router.DELETE("/api/users", handlers.DeleteUserHandler)
 
-	router.GET("/api/users/addresses", handlers.GetUserAddressesHandler)
-	router.POST("/api/users/addresses", handlers.AddAddressToUserHandler)
-	router.PUT("/api/users/addresses", handlers.UpdateUserAddressHandler)
-	// router.PATCH("/api/users/addresses", handlers.DeleteAddressFromUserHandler)
-}
+		router.GET("/api/users/addresses", handlers.GetUserAddressesHandler)
+		router.POST("/api/users/addresses", handlers.AddAddressToUserHandler)
+		router.PUT("/api/users/addresses", handlers.UpdateUserAddressHandler)
+		// router.PATCH("/api/users/addresses", handlers.DeleteAddressFromUserHandler)
+	}
+
+	func SetupWebRoutes(router *gin.Engine) {
+		router.LoadHTMLGlob("web/templates/*.html")
+		router.GET("/", func(c *gin.Context) {
+        	c.HTML(http.StatusOK, "index.html", nil)
+    	})
+		router.GET("/login", gin.WrapF(handlers.GoogleLoginHandler))
+		router.GET("/auth/google/callback", gin.WrapF(handlers.GoogleCallBackHandler))
+		router.GET("/dashboard", gin.WrapF(middleware.AuthMiddleware(handlers.DashboardHandler)))
+		router.GET("/address-book", gin.WrapF(middleware.AuthMiddleware(handlers.AddressBookHandler)))
+
+		router.Static("/static", "./web/static")
+	}
--- main.go
+++ main.go
@@ -1,39 +1,19 @@
 package main
 
 import (
-	"net/http"
-	"os"
-
 	"github.com/anujshah3/AddressTrail/internal/routes"
 	"github.com/gin-gonic/gin"
-	"github.com/gorilla/sessions"
 )
 
 
-
-var store = sessions.NewCookieStore([]byte(os.Getenv("SESSION_SECRET")))
-
-
-func handleIndex(res http.ResponseWriter, req *http.Request) {
-	http.ServeFile(res, req, "web/templates/index.html")
-}
 
 func main() {
     router := gin.Default()
 
 	routes.SetupAPIRoutes(router)
-	
+
+	routes.SetupWebRoutes(router)
+
 	router.Run(":8080")
 
-	// http.HandleFunc("/", handleIndex)
-	// http.HandleFunc("/login", handlers.GoogleLoginHandler)
-	// http.HandleFunc("/auth/google/callback", handlers.GoogleCallBackHandler)
-	// http.HandleFunc("/dashboard", middleware.AuthMiddleware(handlers.DashboardHandler))
-	// http.HandleFunc("/address-book", middleware.AuthMiddleware(handlers.AddressBookHandler))
-
-	// fs := http.FileServer(http.Dir("web/static"))
-	// http.Handle("/static/", http.StripPrefix("/static/", fs))
-
-	// fmt.Println("Server listening on port 8080")
-	// http.ListenAndServe(":8080", nil)
 }
